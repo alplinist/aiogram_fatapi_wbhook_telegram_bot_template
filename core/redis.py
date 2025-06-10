e82073df598b5e895f8d0e5898de0a77a15cec8b
alplinist <147479519+alplinist@users.noreply.github.com>
2025-06-10 15:24:35 +0500

    replace aioredis with redis

--- core/redis.py
+++ core/redis.py
@@ -1,12 +1,12 @@
-import aioredis
+import redis.asyncio as redis
 from .config import REDIS_URL
 
-redis: aioredis.Redis | None = None
+redis_client: redis.Redis | None = None
 
 async def init_redis() -> None:
-    global redis
-    redis = aioredis.from_url(REDIS_URL)
+    global redis_client
+    redis_client = redis.from_url(REDIS_URL)
 
 async def close_redis() -> None:
-    if redis is not None:
-        await redis.close()
+    if redis_client is not None:
+        await redis_client.close()
